51c469ea437d778f1e6e50f9849e23012d9534aa
AdamNgazzou <adem.ng123@gmail.com>
2025-04-09 21:05:37 +0100

    added new project edulearn

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import logo from "../../public/favicon.ico";
 
 import backend from "./backend.png";
@@ -61,3 +62,65 @@ export {
   pitchup,
   edulearn
 };
+=======
+import logo from "../../public/favicon.ico";
+
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import link from "./link.png";
+
+import firebase from "./tech/firebase.svg";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import redis from "./tech/redis.svg";
+import nextjs from "./tech/next.svg";
+import express from "./tech/express.svg"
+
+import highschool from "./company/highschool.png";
+import uni from "./company/uni.png";
+import tesla from "./company/tesla.png";
+
+import pitchup from "./pitchup.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  link,
+  menu,
+  close,
+  firebase,
+  docker,
+  figma,
+  git,
+  javascript,
+  mongodb,
+  nodejs,
+  nextjs,
+  reactjs,
+  tailwind,
+  typescript,
+  redis,
+  express,
+  highschool,
+  uni,
+  tesla,
+  pitchup,
+};
+>>>>>>> b8e5c9d4f444b67d3a1f3a3c028e0f5236067c95
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {
   mobile,
   backend,
@@ -237,3 +238,197 @@ const projects = [
 ];
 
 export { services, technologies, experiences, projects };
+=======
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  firebase,
+  reactjs,
+  express,
+  pitchup,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  highschool,
+  tesla,
+  nextjs,
+  redis,
+  uni,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Computer science student",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "Redis",
+    icon: redis,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "firebase",
+    icon: firebase,
+  },
+
+  {
+    name: "nextJs",
+    icon: nextjs,
+  },
+  {
+    name: "ExpressJs",
+    icon: express,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+
+
+];
+
+const experiences = [
+  {
+    title: "HighSchool Student",
+    company_name: "farhat hached Highschool",
+    icon: highschool,
+    iconBg: "#181c2e",
+    date: "Sep 2020 - juin 2024",
+    points: [
+      "Studied Data Structures and Algorithms as a main course, developing strong problem-solving skills.",
+      "Passionate about web development, built static websites and server-side rendered applications using PHP and MySQL.",
+      "Had a main course in databases and achieved a 16.31/20 overall grade in the Baccalaureate exam.",
+    ],
+  },
+  {
+    title: "Computer Science Student",
+    company_name: "Horizon Digital School of Technologies",
+    icon: uni,
+    iconBg: "#E6DEDD",
+    date: "Sep 2024 - Juin 2027",
+    points: [
+      "Awarded a 100% scholarship for academic excellence, recognizing outstanding performance in my Computer Science degree.",
+      "Participated in Nuit de l’Info, a national hackathon, collaborating on innovative solutions under time constraints.",
+      "Gained expertise in full-stack development, mastering technologies like React, Next.js, Node.js, Express, Redis, and more.",
+      "Consistently maintained high grades while actively developing projects in my free time, balancing academics and hands-on learning.",
+    ],
+  },
+  {
+    title: "Full stack Developer intern",
+    company_name: "Your Company",
+    icon: tesla,
+    iconBg: "#181c2e",
+    date: "- Present",
+    points: [
+      "waiting for you to fulfill this place ^^"
+    ],
+  },
+];
+
+
+
+const projects = [
+  {
+    name: "Pitchup startups",
+    description:
+      "Web-based platform that allows users to pitch, explore, and connect with startups, providing a collaborative space for discovering innovative business ideas.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+        icon: reactjs // Make sure this is imported at the top
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+        icon: mongodb // Make sure this is imported at the top
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+        icon: tailwind // Make sure this is imported at the top
+      },
+    ],
+    image: pitchup,
+    source_code_link: "https://github.com/AdamNgazzou/startups-mern-backend",
+    source_link: "https://pitchup-five.vercel.app"
+  }
+];
+
+export { services, technologies, experiences, projects };
+>>>>>>> b8e5c9d4f444b67d3a1f3a3c028e0f5236067c95
